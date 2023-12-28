d4fcdb27928fa708ad5fdad8d284d06529ac29e8
roshan22418 <roshan22418@iiitd.ac.in>
2023-12-28 23:47:09 +0530

    some function add

--- mystring.cpp
+++ mystring.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
+#include"mystring.h"
+#include<cstring> 
+
 using namespace std;
 
-int main(){
+
+//defination of all declared function i mystring.h
+Mystring::Mystring(){
+    data = new char[1];
+    length1 = 0;
+    data[0] ='\0';
+}
+
+
+Mystring::Mystring(const char *src){
+    data = new char[strlen(src)+1];
     
+    length1 = strlen(src);
+    strcpy(data,src);
+
+}
+
+Mystring::Mystring(const Mystring &src){
+
+}
+
+int main(){
+
 }
--- mystring.h
+++ mystring.h
@@ -4,6 +4,22 @@
 #include<iostream>
 
 
+class Mystring{
+private:
+    char *data;
+    int length1;
+public:
+    Mystring();
+    Mystring(const char *data);
+    Mystring(const Mystring &src);
+    ~Mystring();
+    int length() const;
+    bool isempty();
+
+
+};
+
+
 
 
 
